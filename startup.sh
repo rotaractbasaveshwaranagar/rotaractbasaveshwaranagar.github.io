e51164cca1e8db3b54d701d29424c875f93a0a2c
Chiranthan S <chiras@cisco.com>
2021-01-29 20:44:32 +0530

    Dockerfile fix1

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-cd /app/site && hugo server -D --baseUrl="https://rbb.cisco.com/" --bind=0.0.0.0 --appendPort=false --buildDrafts
+cd /app/site/themes/rbb && hugo server -D --baseUrl="https://rbb.cisco.com/" --bind=0.0.0.0 --appendPort=false --buildDrafts --themesDir ..
