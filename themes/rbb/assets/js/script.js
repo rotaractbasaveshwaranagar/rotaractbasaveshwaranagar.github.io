6a9f9b60fcb0407bdf42478b460799850460e927
Sandya Sivakumar <sandsiva@cisco.com>
2021-02-03 14:59:07 +0530

    ajax calls added

--- themes/rbb/assets/js/script.js
+++ themes/rbb/assets/js/script.js
@@ -29,6 +29,8 @@
     });
   });
 
+
+
   //Hero Slider
   $('.hero-slider').slick({
     autoplay: true,
@@ -102,6 +104,78 @@
       });
     });
   });
+
+  $(document).ready(function() {
+
+      // process the form
+      $('#joinUsForm').submit(function(event) {
+
+          // get the form data
+          // there are many ways to get this data using jQuery (you can use the class or id also)
+          var formData = {
+              'name'       : $('input[name=name]').val(),
+              'mail'       : $('input[name=mail]').val(),
+              'subject'    : $('input[name=subject]').val(),
+              'message'    : $('input[name=message]').val()
+          };
+
+          // process the form
+          $.ajax({
+              type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+              url         : 'https://rbb-data-api.cisco.com/api/v1/rbb/join-us', // the url where we want to POST
+              data        : formData, // our data object
+              dataType    : 'json', // what type of data do we expect back from the server
+              encode          : true
+          })
+              // using the done promise callback
+              .done(function(data) {
+
+                  // log data to the console so we can see
+                  console.log(data);
+
+                  // here we will handle errors and validation messages
+              });
+
+          // stop the form from submitting the normal way and refreshing the page
+          event.preventDefault();
+      });
+
+  });
+
+  $(document).ready(function() {
+
+        // process the form
+        $('#loginForm').submit(function(event) {
+
+            // get the form data
+            // there are many ways to get this data using jQuery (you can use the class or id also)
+            var formData = {
+                'username'              : $('input[name=name]').val(),
+                'password'             : $('input[name=mail]').val()
+            };
+
+            // process the form
+            $.ajax({
+                type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+                url         : 'https://rbb-data-api.cisco.com/api/v1/rbb/login', // the url where we want to POST
+                data        : formData, // our data object
+                dataType    : 'json', // what type of data do we expect back from the server
+                encode          : true
+            })
+                // using the done promise callback
+                .done(function(data) {
+
+                    // log data to the console so we can see
+                    console.log(data);
+
+                    // here we will handle errors and validation messages
+                });
+
+            // stop the form from submitting the normal way and refreshing the page
+            event.preventDefault();
+        });
+
+    });
 
 
 })(jQuery);
