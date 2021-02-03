433147b545e7a041babada7910daa6eda6738b9b
Sandya Sivakumar <sandsiva@cisco.com>
2021-02-03 15:32:47 +0530

    api url updated

--- site/themes/rbb/assets/js/script.js
+++ site/themes/rbb/assets/js/script.js
@@ -157,7 +157,7 @@
             // process the form
             $.ajax({
                 type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
-                url         : 'https://rbb-data-api.cisco.com/api/v1/rbb/login', // the url where we want to POST
+                url         : 'https://rbb-data-api.cisco.com/api/v1/rbb/member-login', // the url where we want to POST
                 data        : formData, // our data object
                 dataType    : 'json', // what type of data do we expect back from the server
                 encode          : true
