eabc30653fb42d3e33d4d0d07eb5ccaee0f82b22
Sandya Sivakumar <sandsiva@cisco.com>
2021-02-03 17:13:47 +0530

    allow origin

--- site/themes/rbb/assets/js/script.js
+++ site/themes/rbb/assets/js/script.js
@@ -160,6 +160,7 @@
                 url         : 'https://rbb-data-api.cisco.com/api/v1/rbb/member-login', // the url where we want to POST
                 headers: {
                         'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*',
                 },
                 data        : formData, // our data object
                 dataType    : 'json', // what type of data do we expect back from the server
